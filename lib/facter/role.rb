a84181426b14b2735f830b833f09d2cca74d34da
Valera Meleshko <vmeleshko@minsk.ximxim.com>
2015-12-08 15:04:43 +0300

    added new role "proj"

--- lib/facter/role.rb
+++ lib/facter/role.rb
@@ -2,9 +2,11 @@ if Facter.value(:hostname) =~ /^(\w+)-(\w+)-\d+$/
   Facter.add('role') do
     setcode {$2}
   end
-
 elsif Facter.value(:hostname) =~ /^(\w+)-(\w+)$/
   Facter.add('role') do
     setcode {$2}
+  end
+  Facter.add('proj') do
+    setcode {$1}
   end
 end
